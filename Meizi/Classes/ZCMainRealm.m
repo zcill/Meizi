e6a397a5e5d5c4b1cbcc8345adedd0596620efba
zcill <zcillcoder@gmail.com>
2016-01-12 22:46:46 +0800

    加入Realm用于本地缓存

---
+++ Meizi/Classes/ZCMainRealm.m
@@ -0,0 +1,27 @@
+//
+//  ZCMainRealm.m
+//  Meizi
+//
+//  Created by 朱立焜 on 16/1/12.
+//  Copyright © 2016年 朱立焜. All rights reserved.
+//
+
+#import "ZCMainRealm.h"
+
+@implementation ZCMainRealm
+
+// Specify default values for properties
+
+//+ (NSDictionary *)defaultPropertyValues
+//{
+//    return @{};
+//}
+
+// Specify properties to ignore (Realm won't persist these)
+
+//+ (NSArray *)ignoredProperties
+//{
+//    return @[];
+//}
+
+@end
